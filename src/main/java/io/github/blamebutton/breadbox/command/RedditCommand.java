a18d59d52387f54c710b4f8a61d6f57831aefadc
Raphaël Bunck <raphael@bunck.me>
2018-07-08 00:53:39 +0200

    Fixed options, will work on rest later

--- src/main/java/io/github/blamebutton/breadbox/command/RedditCommand.java
+++ src/main/java/io/github/blamebutton/breadbox/command/RedditCommand.java
@@ -2,6 +2,8 @@ package io.github.blamebutton.breadbox.command;
 
 import io.github.blamebutton.breadbox.util.I18n;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import sx.blah.discord.handle.obj.IMessage;
 
 @BreadboxCommand("reddit")
@@ -9,7 +11,6 @@ public class RedditCommand implements ICommand {
 
     @Override
     public void handle(IMessage message, CommandLine commandLine) {
-
     }
 
     @Override
@@ -20,5 +21,15 @@ public class RedditCommand implements ICommand {
     @Override
     public String getDescription() {
         return I18n.get("command.reddit.description");
+    }
+
+    @Override
+    public Options getOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder("q").argName("query").hasArg(true).desc("Search query string of max 512 characters").build());
+        options.addOption(Option.builder("tp").argName("type").hasArg(true).desc("Search type (sr, link, user)").build());
+        options.addOption(Option.builder("t").argName("time").hasArg(true).desc("Search time (hour, day, week, month, all)").build());
+        options.addOption(Option.builder("s").argName("sort").hasArg(true).desc("Search sort (relevance, hot, top, new, comments)").build());
+        return options;
     }
 }
